d8b239810714a5a2a24a186cb76af8f46ebe503e
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-09-16 20:43:55 +0530

    Stream Changes

--- client/src/Components/Stream/stream.jsx
+++ client/src/Components/Stream/stream.jsx
@@ -3,7 +3,7 @@ import "./deny.css";
 
 function Stream(props){
     const prevDuration = props.Duration;
-    // create a timer starting from (total duration(45) - prev duration)  use this --> https://www.npmjs.com/package/react-countdown
+    // create a timer starting from (total duration(45) - prev duration)  use this --> https://www.npmjs.com/package/react-countdown or any other suitable
     // On countdown to zero, updateDuration in database && then shutdown the window
     // create a video window
     return(
