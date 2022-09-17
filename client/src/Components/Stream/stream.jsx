076f17f55fd58894d82d25acbcaaf9753b9759c4
ANIRUDH <anirudh4656@gmail.com>
2022-09-18 02:10:40 +0530

    f

--- client/src/Components/Stream/stream.jsx
+++ client/src/Components/Stream/stream.jsx
@@ -1,16 +1,42 @@
 import React from "react";
-import "./deny.css";
+import { useEffect ,useState} from "react";
+// import "./deny.css";
 
 function Stream(props){
     const prevDuration = props.Duration;
+    const [seconds, setSeconds] =useState(60);
+    const [minutes, setMinutes] =useState(45);
+    const [total, setTotal] =useState(0);
+    var timer;
+    useEffect(()=>{
+
+        timer = setInterval(()=>{
+            setSeconds(seconds-1);
+            if(seconds===0){
+                setMinutes(minutes-1);
+                setSeconds(60);
+            }
+        },1000)
+        return ()=>clearInterval(timer)
+    },[seconds])
+    if(minutes===0){
+        console.log("time exceed");
+        clearInterval(timer)
+    }
+    
+   
+
+  
     // create a timer starting from (total duration(45) - prev duration)  use this --> https://www.npmjs.com/package/react-countdown or any other suitable
+  
     // On countdown to zero, updateDuration in database && then shutdown the window
     // create a video window
     return(
         <div className="stream-section">
         <div className="video-div">
-        
+    
         </div>
+        <h1> {minutes}:{seconds}</h1> 
         <div className="timer-div">
         
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
-
+import Stream from "./Components/Stream/stream";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+{/* <Stream /> */}
   </React.StrictMode>
 );
 
