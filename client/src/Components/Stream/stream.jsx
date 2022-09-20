6f7a4612351344c6abc6fefe88614aa339d53d44
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-09-20 16:43:02 +0530

    Timer

--- client/src/Components/Stream/stream.jsx
+++ client/src/Components/Stream/stream.jsx
@@ -6,65 +6,27 @@ import "./stream.css";
 
 function Stream(props){
 
-    function getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-          }
-        }
-        return "";
-      }
-    console.log(props.values);
-    const [seconds, setSeconds] =useState(props.values.Seconds);
-    const [minutes, setMinutes] =useState(props.values.Minutes);
-    // const [mail, setMail] =useState(props.values.Email);
-    const address = getCookie("email");
+    const [seconds, setSeconds] =useState(59);
+    const [minutes, setMinutes] =useState(44);
     var timer;
 
     const sizeContainerRef = React.useRef();
 const containerRef = React.useRef();
 const videoRef = React.useRef();
 
-window.addEventListener("beforeunload",function(event){
-    event.preventDefault();
-    axios.post('/updateduration',{
-        Email: address,
-        Duration: {
-            Minutes: minutes,
-            Seconds: seconds
-        }
-    }).then((res)=>{
-        if(res.data.Message === "Success"){
-            alert("Stream Closed");
-        }
-        else{
-            alert("Error Occurred");
-        }
-    });
-    console.log("Stream Closed!!!");
-    alert("Stream Closed !!!");
-});
-
 useEffect(()=>{
-            // const args = {
-        //     address: "wss://share.ragnarok.arcware.cloud/12c0cfd9-8f8f-41b7-a21d-e10e1019e8d5",
-        //     packageId: "Name of the package (if there are multiple applications)",
-        //     settings: {},
-        //     sizeContainer: sizeContainerRef.current,
-        //     container: containerRef.current,
-        //     videoRef: videoRef.current,
-        //     playOverlay: true, // Set default overlay with play button. Make it false and use loader
-        //     loader: (val) => {}, // Callback for loading screen, etc. Once stream is ready, the function will be triggered with false value.
-        //     applicationResponse: (response) => {}, // Callback for Unreal Engine application messages.
-        //   };
-        //   const webrtc_client = new WebRTCClient(args);
+//             const args = {
+//             address: "wss://share.ragnarok.arcware.cloud/12c0cfd9-8f8f-41b7-a21d-e10e1019e8d5",
+//             packageId: "Name of the package (if there are multiple applications)",
+//             settings: {},
+//             sizeContainer: sizeContainerRef.current,
+//             container: containerRef.current,
+//             videoRef: videoRef.current,
+//             playOverlay: true, // Set default overlay with play button. Make it false and use loader
+//             loader: (val) => {}, // Callback for loading screen, etc. Once stream is ready, the function will be triggered with false value.
+//             applicationResponse: (response) => {}, // Callback for Unreal Engine application messages.
+//           };
+//           const webrtc_client = new WebRTCClient(args);
 // const emitUIInteraction = webrtc_client.emitUIInteraction;
 })
  
@@ -82,19 +44,9 @@ useEffect(()=>{
     },[seconds])
     if(minutes===0 && seconds===0){
         console.log("time exceed");
-        axios.post('/updateduration',{
-            Email: props.values.Email,
-            Duration: {
-                Minutes: 0,
-                Seconds: 0
-            }
-        }).then((res)=>{
-            if(res.data.Message === "Success"){
-                alert("Stream Time Exceeded");
-                window.close();
-            }
-        });
         clearInterval(timer)
+        alert("Stream Time Exceeded");
+        window.close();
     }
 
     return(
