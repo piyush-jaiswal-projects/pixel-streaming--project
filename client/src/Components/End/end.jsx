8d4c00c2b849731a097005094f96f1802c73131f
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-10-05 10:04:29 +0530

    Bug Fixed

--- client/src/Components/End/end.jsx
+++ client/src/Components/End/end.jsx
@@ -6,24 +6,24 @@ function Deny({language}){
         <div className="register-section">
         {language?  <div className="register-div">
         <div className="form-container" style={{margin:"auto", position:"relative", top:"50px"}}>
-        <p style={{textAlign:"left",fontFamily:"sans-serif", margin:"0", fontWeight:"bold"}}>
+        <p style={{textAlign:"left",fontFamily:"sans-serif", margin:"0", fontWeight:"bold", color:"#FFFFFF"}}>
             You have watched stream for 45 minutes. To continue watching, please login again.
         </p>
         <br></br>
         <Link to='/login'>
-        <button className="register-button">Request Access</button>
+        <button className="register-button">LOG IN AGAIN</button>
         </Link>
         </div>
         </div>:<>
         {/* belgium */}
         <div className="register-div">
         <div className="form-container" style={{margin:"auto", position:"relative", top:"50px"}}>
-        <p style={{textAlign:"left",fontFamily:"sans-serif", margin:"0", fontWeight:"bold"}}>
-            You have watched stream for 45 minutes. To continue watching, please login again.other language
+        <p style={{textAlign:"left",fontFamily:"sans-serif", margin:"0", fontWeight:"bold", color:"#FFFFFF"}}>
+        Du har tittat på stream i 45 minuter. Logga in igen för att fortsätta titta.
         </p>
         <br></br>
         <Link to='/login'>
-        <button className="register-button">Request Access</button>
+        <button className="register-button">LOGGA IN IGEN</button>
         </Link>
         </div>
         </div>
