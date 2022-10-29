4f8a870bd40515ba6837cd9b7bd8d4b6a73314d1
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-10-29 10:20:36 +0530

    Timer Duration From Database Added

--- controllers/startTimer.js
+++ controllers/startTimer.js
@@ -2,15 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userSchema = require("../database/database.js");
 const {streamSwitch} = require('./adminPanel/switching.js');
+const {sessionSchema} = require('../database/schemas.js');
+const Session = mongoose.model("Session", sessionSchema);
 
 
 const User = mongoose.model("User", userSchema);
 
 async function StartServer(req, res){
+    var timerDuration;
+    const promise = await Session.findOne({User: "Admin"})
+    .then(function(foundUser){
+        const timer = foundUser.Duration;
+        timerDuration = timer*60*1000;
+    });
+    console.log(timerDuration);
     console.log("Timer Started");
     setInterval(function() {
         res.send('Timer Ended');
-    },20000);
+    },timerDuration);
     }
 
 module.exports = StartServer;
